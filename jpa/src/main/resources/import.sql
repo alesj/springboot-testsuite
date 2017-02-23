4211d4a53f9d4b4fea717be0a0440ce1eda5137b
Gytis Trikleris <gytis@redhat.com>
2017-02-23 14:59:55 +0100

    JPA test without integration assertions

---
+++ jpa/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into band(name) values ('AC/DC')
+insert into record(band_id, name) values(1, 'High Voltage')
+insert into record(band_id, name) values(1, 'T.N.T.')
+
+insert into band(name) values ('Iron Maiden')
+insert into record(band_id, name) values(2, 'Fear of the Dark')
